7a9044ab1fd104915d2a597a95be86e011847217
rjswh0503 <rjswh0503@yahoo.com>
2025-02-17 19:38:06 +0900

    mongodbConnection

--- mongo.js
+++ mongo.js
@@ -1,7 +1,24 @@
+const MongoClient = require('mongodb').MongoClient
+
+const url ='mongodb+srv://shin:153123@cluster0.ydxf4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 const createProduct = async(req, res, next) => {
+    const newProduct = {
+        name: req.body.name,
+        price: req.body.price
+    };
 
+    const client = new MongoClient(url);
+
+    try{
+        await client.connect();
+        const db = client.db();
+        const result = db.collection('products').insertOne(newProduct)
+    }catch (error){
+        return res.json({message: '저장된 데이터가 없습니다.'});
+    }
 };
+
 
 
 const getProducts = async(req,res,next) => {
