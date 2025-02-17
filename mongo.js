7de58467ce4ad8eeba1092401938920936300f5a
rjswh0503 <rjswh0503@yahoo.com>
2025-02-17 20:12:31 +0900

    mongodb에 post,get 데이터 추가하기

--- mongo.js
+++ mongo.js
@@ -12,17 +12,30 @@ const createProduct = async(req, res, next) => {
 
     try{
         await client.connect();
-        const db = client.db();
+        const db = client.db('products_test');
         const result = db.collection('products').insertOne(newProduct)
     }catch (error){
-        return res.json({message: '저장된 데이터가 없습니다.'});
-    }
+        return res.json({message: '데이터 저장 실패!!'});
+    };
+    
+
+    res.json(newProduct);
 };
 
 
 
 const getProducts = async(req,res,next) => {
+    const client = new MongoClient(url);
+    let products;
+    try{
+        await client.connect();
+        const db = client.db('products_test');
+        products = await db.collection('products').find().toArray();
+    }catch(error){
+        return res.json({message: '저장된 데이터가 없습니다.'});
+    };
 
+    res.json(products);
 };
 
 
